4fd985520b29c801269813bc4a04a72e1d13759d
Humano Laranja <contato.carlos@outlook.com>
2022-12-05 22:04:34 -0300

    remove msg confirm

--- udp/client.c
+++ udp/client.c
@@ -7,47 +7,50 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-	
-#define PORT	 8080
+
+#define PORT 8080
 #define MAXLINE 1024
-	
+
 // Driver code
-int main() {
+int main()
+{
 	int sockfd;
 	char buffer[MAXLINE];
-    char message[MAXLINE];
+	char message[MAXLINE];
 	char *hello = "Hello from client";
-	struct sockaddr_in	 servaddr;
-	
+	struct sockaddr_in servaddr;
+
 	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	memset(&servaddr, 0, sizeof(servaddr));
-		
+
 	// Filling server information
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
-		
+
 	int n, len;
-	
-    while(strcmp(buffer, "finalizar_chat") != 0){
-        printf("Type message to send: ");
-        scanf("%s", &message);
-        sendto(sockfd, (const char *)message, strlen(message),
-	    	MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-	    		sizeof(servaddr));
-    
-	    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-	    			MSG_WAITALL, (struct sockaddr *) &servaddr,
-	    			&len);
-	    buffer[n] = '\0';
-	    printf("Server: %s\n", buffer);
-    }
-	
+
+	while (strcmp(buffer, "finalizar_chat") != 0)
+	{
+		printf("Type message to send: ");
+		scanf("%s", &message);
+		sendto(sockfd, (const char *)message, strlen(message),
+			   0, (const struct sockaddr *)&servaddr,
+			   sizeof(servaddr));
+
+		n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+					 MSG_WAITALL, (struct sockaddr *)&servaddr,
+					 &len);
+		buffer[n] = '\0';
+		printf("Server: %s\n", buffer);
+	}
+
 	close(sockfd);
 	return 0;
 }
--- udp/server.c
+++ udp/server.c
@@ -7,58 +7,61 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-	
-#define PORT	 8080
+
+#define PORT 8080
 #define MAXLINE 1024
-	
+
 // Driver code
-int main() {
+int main()
+{
 	int sockfd;
 	char buffer[MAXLINE];
-    char message[MAXLINE];
+	char message[MAXLINE];
 	char *hello = "Hello from server";
 	struct sockaddr_in servaddr, cliaddr;
-		
+
 	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-		
+
 	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
-		
+
 	// Filling server information
 	servaddr.sin_family = AF_INET; // IPv4
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
-		
+
 	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-			sizeof(servaddr)) < 0 )
+	if (bind(sockfd, (const struct sockaddr *)&servaddr,
+			 sizeof(servaddr)) < 0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-		
+
 	int len, n;
-	
-	len = sizeof(cliaddr); //len is value/result
+
+	len = sizeof(cliaddr); // len is value/result
 
 	printf("Waiting for message...");
-    while(strcmp(buffer, "finalizar_chat") != 0){
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-	    			MSG_WAITALL, (struct sockaddr *) &servaddr,
-	    			&len);
-	    buffer[n] = '\0';
-	    printf("Server: %s\n", buffer);
+	while (strcmp(buffer, "finalizar_chat") != 0)
+	{
+		n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+					 MSG_WAITALL, (struct sockaddr *)&servaddr,
+					 &len);
+		buffer[n] = '\0';
+		printf("Server: %s\n", buffer);
 
-        printf("Type message to send: ");
-        scanf("%s", &message);
-        sendto(sockfd, (const char *)message, strlen(message),
-	    	MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-	    		sizeof(servaddr));
-    }
+		printf("Type message to send: ");
+		scanf("%s", &message);
+		sendto(sockfd, (const char *)message, strlen(message),
+			   0, (const struct sockaddr *)&servaddr,
+			   sizeof(servaddr));
+	}
 
 	return 0;
 }
